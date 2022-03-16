0c8469a374b3ab1b6914580cb4602a79dfc80a4c
luisDSotoE <ldsoto@unicesar.edu.co>
2022-03-16 09:39:08 -0500

    taller2
    punto1

---
+++ proyecto/src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto;
+public class Proyecto {
+
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.printf("x = %d\n", x);
+        System.out.printf("El valor de %d + %d es %d\n", x, x, ( x + x ) );
+        System.out.printf("El valor de %d / 2 = %d\n", x, (x / 2));
+        System.out.printf("El valor de %d mod 3 = %d\n", x, x%3);
+    }
+}
